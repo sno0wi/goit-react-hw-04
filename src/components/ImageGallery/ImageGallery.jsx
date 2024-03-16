47d78f0b5c0d5e85634a593ec2d197dfb735ec21
sno0wi <sno0wi20@gmail.com>
2024-03-16 18:39:56 +0200

    fixed Gallery

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -4,17 +4,13 @@ import css from "./ImageGallery.module.css";
 const ImageGallery = ({ photos, handleSelectPhoto }) => {
   return (
     <ul className={css.gallery_list}>
-      <li>
-        {photos !== null &&
-          Array.isArray(photos) &&
-          photos.map((photo) => (
-            <ImageCard
-              img={photo}
-              key={photo.id}
-              handleSelectPhoto={handleSelectPhoto}
-            />
-          ))}
-      </li>
+      {photos !== null &&
+        Array.isArray(photos) &&
+        photos.map((photo) => (
+          <li key={photo.id}>
+            <ImageCard img={photo} handleSelectPhoto={handleSelectPhoto} />
+          </li>
+        ))}
     </ul>
   );
 };
